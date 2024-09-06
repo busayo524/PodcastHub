299ce40c15d34825e442fda76639a382bd72384b
busayo524 <olowookerebusayo66@gmail.com>
2024-09-06 15:28:58 +0100

    loginandsignup

--- forms.py
+++ forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
-                           validators=[DataRequired(), Length(min=2, max=22)])
+                           validators=[DataRequired(), Length(min=3, max=20)])
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -12,9 +12,9 @@ class RegistrationForm(FlaskForm):
                                      validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
 
-class LoginForms(FlaskForm):
+class LoginForm(FlaskForm):
     email = StringField('Email',
                         validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember Me')
-    submit = SubmitField('Log in')
+    submit = SubmitField('Login')
