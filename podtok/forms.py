2301744240f2101f89de7f54a122ccc8297cd28f
busayo524 <olowookerebusayo66@gmail.com>
2024-09-10 12:12:39 +0100

    requirementfile

--- podtok/forms.py
+++ podtok/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
-from podtok.models import user
+from podtok.models import User
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username',
